f52cf8fcd4cc7e101b524e5b76aa9857416eb74c
Mohab Akram <mohapmobile@gmail.com>
2020-08-13 14:33:41 +0200

    bubble sort method

---
+++ bubble_sort.rb
@@ -0,0 +1,26 @@
+ 
+arr1 = [8,9,2,3,5]
+
+# def bubble_sort(arr)
+#   go_throught_counter = 0
+#     loop do 
+#             arr.each_with_index do |n,i|
+#                 break if arr[i+1] == nil
+#                 if arr[i+1] < n
+#                     arr[i] = arr[i+1]
+#                     arr[i+1] = n
+#                 else 
+#                     next
+#                 end
+#             end
+#         go_throught_counter += 1
+#         break if arr.length - 1 == go_throught_counter 
+#     end
+#    arr
+# end
+
+
+p bubble_sort(arr1)
+
+
+
