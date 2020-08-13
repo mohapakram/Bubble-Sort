f1545e036cfbca075025836d865701a910a160c4
Mohab Akram <mohapmobile@gmail.com>
2020-08-13 15:44:28 +0200

    fix linitng errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,49 +1,40 @@
- 
-arr1 = [8,9,2,3,5]
 
 def bubble_sort(arr)
   go_throught_counter = 0
-    loop do 
-            arr.each_with_index do |n,i|
-                break if arr[i+1] == nil
-                if arr[i+1] < n
-                    arr[i] = arr[i+1]
-                    arr[i+1] = n
-                else 
-                    next
-                end
-            end
-        go_throught_counter += 1
-        break if arr.length - 1 == go_throught_counter 
+  loop do
+    arr.each_with_index do |n, i|
+      break if arr[i + 1].nil?
+      if arr[i + 1] < n
+        arr[i] = arr[i + 1]
+        arr[i + 1] = n
+      end
     end
- p  arr
+    go_throught_counter += 1
+    break if arr.length - 1 == go_throught_counter
+  end
+  p arr
 end
 
-# bubble_sort(arr1)
-
+bubble_sort([8, 9, 2, 3, 5])
 
 def bubble_sort_by(arr)
-go_throught_counter = 0
-    loop do 
-            arr.each_with_index do |n,i|
-                break if arr[i+1] == nil
-                diff = yield(n , arr[i+1]) 
-                if diff > 0
-                    arr[i] = arr[i+1]
-                    arr[i+1] = n
-                else 
-                    next
-                end
-            end
-        go_throught_counter += 1
-        break if arr.length - 1 == go_throught_counter 
+  go_throught_counter = 0
+  loop do
+    arr.each_with_index do |n, i|
+      break if arr[i + 1].nil?
+      diff = yield(n, arr[i + 1])
+      if diff > 0
+        arr[i] = arr[i + 1]
+        arr[i + 1] = n
+      end
     end
+    go_throught_counter += 1
+    break if arr.length - 1 == go_throught_counter
+  end
   p arr
-end 
+end
 
-
-
-bubble_sort_by(["hi","hello","hey"]) do |left,right|
+bubble_sort_by(%w[hi hello hey]) do |left, right|
   left.length - right.length
 end
 # => ["hi", "hey", "hello"]
