1797464c4c8f900beddb48412c5ffd1b55fcb61f
Mohab Akram <mohapmobile@gmail.com>
2020-08-13 15:00:33 +0200

    bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -16,10 +16,34 @@ def bubble_sort(arr)
         go_throught_counter += 1
         break if arr.length - 1 == go_throught_counter 
     end
-   arr
+ p  arr
 end
 
-p bubble_sort(arr1)
+# bubble_sort(arr1)
+
+
+def bubble_sort_by(arr)
+go_throught_counter = 0
+    loop do 
+            arr.each_with_index do |n,i|
+                break if arr[i+1] == nil
+                diff = yield(n , arr[i+1]) 
+                if diff > 0
+                    arr[i] = arr[i+1]
+                    arr[i+1] = n
+                else 
+                    next
+                end
+            end
+        go_throught_counter += 1
+        break if arr.length - 1 == go_throught_counter 
+    end
+  p arr
+end 
 
 
 
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
+# => ["hi", "hey", "hello"]
